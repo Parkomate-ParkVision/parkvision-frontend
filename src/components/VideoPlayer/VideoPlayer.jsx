3eb3069bcab19729ec85449c776c8a7ad2171dfa
atharvabhide <atharvabhide91@gmail.com>
2024-05-29 22:56:57 +0530

    second last frontend push

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,31 +1,32 @@
-import { ApiConfig } from "../../utils/config";
+import { ApiConfig } from '../../utils/config';
 
 const VideoStream = ({ url }) => {
-  const videoUrl = String(url)
-    .replace("://", "%3A%2F%2F")
-    .replace("/", "%2F")
-    .replace(":", "%3A")
-    .replace("@", "%40")
-    .replace("?", "%3F")
-    .replace("=", "%3D")
-    .replace("&", "%26");
-  return (
-    <div>
-      <div
-        style={{
-          margin: "0px",
-          height: "100%",
-          "background-color": "black",
-        }}
-      >
-        <img
+	const videoUrl = String(url)
+		.replace('://', '%3A%2F%2F')
+		.replace('/', '%2F')
+		.replace(':', '%3A')
+		.replace('@', '%40')
+		.replace('?', '%3F')
+		.replace('=', '%3D')
+		.replace('&', '%26');
+	return (
+		<div>
+			<div
+				style={{
+					margin: '0px',
+					height: '100%',
+					'background-color': 'black',
+				}}
+			>
+				{/* <img
           src={`${ApiConfig.cctvFeed}?url=${videoUrl}`}
           width="798"
           height="449"
-        />
-      </div>
-    </div>
-  );
+        /> */}
+				<video width={798} height={449} src="../../assets/24-05-2024_exit_compilation.mp4" type='video/mp4' controls autoPlay/>
+			</div>
+		</div>
+	);
 };
 
 export default VideoStream;
